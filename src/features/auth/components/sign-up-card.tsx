465ad2a0cdd85fb8401d279567e8b8dfaa4a7b05
mrtrq <tarreq.maulana@gmail.com>
2024-12-02 12:51:04 +0700

    feat: sign in & sign up page components

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,27 +1,47 @@
 import {FcGoogle} from "react-icons/fc"
 import {FaGithub} from "react-icons/fa"
 import { DottedSeparator } from "@/components/dotted-separator";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 
-export const SignInCard = () => {
+export const SignUpCard = () => {
     return ( 
         <Card className="w-full h-full md:w-[487px] border-none shadow-none">
             <CardHeader className="flex items-center justify-center text-center p-7">
                 <CardTitle className="text-2xl">
-                    Welcome back!
+                    Sign Up
                 </CardTitle>
+                <CardDescription>
+                    By signing up, you agree to our {" "}
+                    <Link href="/privacy">
+                    <span className="text-blue-700">Privacy policy</span>
+                    </Link> {" "}
+                    and
+                    <Link href="/terms">
+                    <span className="text-blue-700">Terms of service</span>
+                    </Link>
+
+                </CardDescription>
             </CardHeader>
             <div className="px-7 mb-2">
                 <DottedSeparator />
             </div>
             <CardContent className="p-7">
                 <form className="space-y-4">
+                     <Input 
+                    required
+                    type="text"
+                    value={""}
+                    onChange={() => {}}
+                    placeholder="Enter your name"
+                    disabled={false}
+                    />
                     <Input 
                     required
-                    type="email"
+                    type="text"
                     value={""}
                     onChange={() => {}}
                     placeholder="Enter email address"
@@ -67,4 +87,4 @@ export const SignInCard = () => {
     );
 }
  
-export default SignInCard;
+export default SignUpCard;
